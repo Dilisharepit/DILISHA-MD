7adaaf7fb546a16bd1b6b9d8a33bdcaadc80f2e9
Herokumine <whatsappbot11000@gmail.com>
2024-08-17 14:48:43 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -9,7 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if (!q) return reply("please give me a url or tittle")
+if (!q) return reply("please give me a url or title")
 const search = await yts(q)
 const data = search.videos[0];
 const url  = data.url
@@ -17,7 +17,7 @@ const url  = data.url
 let desc = `
 😈 *ᎠᏆᏞᏆՏᎻᎪ-ᎷᎠ song downloader*😈
 
-tittle: ${data.tittle}
+tittle: ${data.title}
 despcription: ${data.description}
 time: ${data.time}
 ago: ${data.ago}
@@ -34,8 +34,8 @@ let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
 // send audio + document message
-await conn.sendmessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"}quoted:mek})
-await conn.sendmessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.tittle + ".mp3",caption:"MADE IN DILISHA💞}quoted:mek})
+await conn.sendmessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},quoted:mek})
+await conn.sendmessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE IN DILISHA💞}quoted:mek})
 
 
 
@@ -55,7 +55,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if (!q) return reply("please give me a url or tittle")
+if (!q) return reply("please give me a url or title")
 const search = await ytv(q)
 const data = search.videos[0];
 const url  = data.url
@@ -80,8 +80,8 @@ let down = await fg.ytv(url)
 let downloadUrl = down.dl_url
 
 // send video + document message
-await conn.sendmessage(from,{video: {url:downloadUrl},mimetype:"video/mp4",caption:"MADE BY DILISHA💞}quoted:mek})
-await conn.sendmessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.tittle + ".mp4"}quoted:mek})
+await conn.sendmessage(from,{video: {url:downloadUrl},mimetype:"video/mp4",caption:"MADE BY DILISHA💞},quoted:mek})
+await conn.sendmessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4"},quoted:mek})
 
 
 
