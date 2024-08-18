db46bc44fbb44711029f61b214af640330ce2f5e
Herokumine <whatsappbot11000@gmail.com>
2024-08-18 06:57:44 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -35,7 +35,7 @@ let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
 // send audio + document message
-await conn.sendmessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendmessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek});
 await conn.sendmessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"MADE IN DILISHA💞"},{quoted:mek})
 
 
