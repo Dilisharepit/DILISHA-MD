ff907c31cc23db06321d2439ac60f2e57759f21a
Herokumine <whatsappbot11000@gmail.com>
2024-08-20 07:26:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"I AM ALIVE NOW 💫",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS ||"true",
+MODE: process.env.MODE ||"public",
 };
